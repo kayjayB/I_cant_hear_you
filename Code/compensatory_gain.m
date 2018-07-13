906b372a7de6c65302b53aad72edde05b56b0ad4
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-07-13 17:57:05 +0200

    Gain working! yayyyyy

--- Code/compensatory_gain.m
+++ Code/compensatory_gain.m
@@ -7,9 +7,6 @@ Sine3 = dsp.SineWave('Frequency',500,'SampleRate',samplingRate);
 
 filter24 = filterBand24;
 filter25 = filterBand25;
-filter26 = filterBand26;
-filter27 = filterBand27;
-filter28 = filterBand28;
 
 %fvtool(filter24, 1, filter25, 1, filter26, 1, filter27, 1, filter28);
 
@@ -43,6 +40,20 @@ for i = 1 : 100
 end
 release(SpecAna)
 
+%% Audiogram 
+frequencies = [250 500 1000 2000 4000 6000 8000];
+values = [15 10 10 15 10 5 20];
+requiredFrequencies = linspace(1, 8*10^3, 8*10^3);
+vq = interp1(frequencies,values,requiredFrequencies);
+figure(1)
+plot(requiredFrequencies, vq);
+audiogramdB = [vq(250), vq(315), vq(400), vq(500), vq(630), vq(800), ... 
+vq(1000), vq(1250), vq(1600), vq(2000), vq(2500), vq(3150), ...   
+vq(4000), vq(5000), vq(6300), vq(8000)];
+audiogram=zeros(1,16);
+for i=1: length(audiogramdB)
+    audiogram(i) = db2mag(audiogramdB(i));
+end
 
 %% Filtering and playing an audio clip
 
@@ -50,14 +61,12 @@ audioInput = dsp.AudioFileReader;
 audioWriter = audioDeviceWriter('SampleRate',audioInput.SampleRate);
 
 % To have a measure of reference, play the original audio once
-% while ~isDone(audioInput)
-%     input = audioInput();  % Load a frame
-%     audioWriter(input);  % Play the frame
-% end
+ while ~isDone(audioInput)
+     input = audioInput();  % Load a frame
+     audioWriter(input);  % Play the frame
+ end
 % Wait until audio is played to the end
 
-pause(10*audioInput.SamplesPerFrame/audioInput.SampleRate);  
-reset(audioInput);         % Reset to beginning of the file
 % release(audioInput);       % Close input file
 % release(audioWriter);               % Close audio output device
 
@@ -66,6 +75,17 @@ filter25 = filterBand25;
 filter26 = filterBand26;
 filter27 = filterBand27;
 filter28 = filterBand28;
+filter29 = filterBand29;
+filter30 = filterBand30;
+filter31 = filterBand31;
+filter32 = filterBand32;
+filter33 = filterBand33;
+filter34 = filterBand34;
+filter35 = filterBand35;
+filter36 = filterBand36;
+filter37 = filterBand37;
+filter38 = filterBand38;
+filter39 = filterBand39;
 
 SpecAna = dsp.SpectrumAnalyzer('PlotAsTwoSidedSpectrum',false, ...
     'SampleRate',audioInput.SampleRate, ...
@@ -76,16 +96,43 @@ SpecAna = dsp.SpectrumAnalyzer('PlotAsTwoSidedSpectrum',false, ...
 while ~isDone(audioInput)
     input = audioInput();  % Load a frame of audio
     
-     y = filter24(input);
-     z = filter25(input);
-     y = y*10; %How to multiply by a factor
-     z = z*5;
-     reconstructed = y + z;
+     a = filter24(input);
+     a = a * audiogram(1);
+     b = filter25(input);
+     b = b * audiogram(2);
+     c = filter26(input);
+     c = c * audiogram(3);
+     d = filter27(input);
+     d = d * audiogram(4);
+     e = filter28(input);
+     e = e * audiogram(5);
+     f = filter29(input);
+     f = f * audiogram(6);
+     g = filter30(input);
+     g = g * audiogram(7);
+     h = filter31(input);
+     h = h * audiogram(8);
+     i = filter32(input);
+     i = i * audiogram(9);
+     j = filter33(input);
+     j = j * audiogram(10);
+     k = filter34(input);
+     k = k * audiogram(11);
+     l = filter35(input);
+     l = l * audiogram(12);
+     m = filter36(input);
+     m = m * audiogram(13);
+     n = filter37(input);
+     n = n * audiogram(14);
+     o = filter38(input);
+     o = o * audiogram(15);
+     p = filter39(input);
+     p = p * audiogram(16);
+
+     reconstructed = a+ b+ c+ d +e+ f+ g+ h+ i+ j+ k+ l+ m+ n+ o+ p;
      
      % Play the reconstructed audio frame
      audioWriter(reconstructed);
      SpecAna(input);
     
 end
-
-hello = 'done'

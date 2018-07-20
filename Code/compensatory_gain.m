79ae7d5950e02781e9ae6dcafb1cb4735ec0961a
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-07-20 15:27:46 +0200

    commit

--- Code/compensatory_gain.m
+++ Code/compensatory_gain.m
@@ -1,11 +1,12 @@
 %%  Filtering and playing an audio clip %%
+Fs= 40000; % sampling frequency (Hz)
 %% Audiogram 
 frequencies = [250 500 1000 2000 4000 6000 8000];
 values = [15 10 10 15 10 5 20];
 requiredFrequencies = linspace(1, 8*10^3, 8*10^3);
 vq = interp1(frequencies,values,requiredFrequencies);
-figure(1)
-plot(requiredFrequencies, vq);
+%figure(1)
+%plot(requiredFrequencies, vq);
 audiogramdB = [vq(250), vq(315), vq(400), vq(500), vq(630), vq(800), ... 
 vq(1000), vq(1250), vq(1600), vq(2000), vq(2500), vq(3150), ...   
 vq(4000), vq(5000), vq(6300), vq(8000)];
@@ -14,90 +15,104 @@ for i=1: length(audiogramdB)
     audiogram(i) = db2mag(audiogramdB(i));
 end
 
-%% Filters
-filter24 = filterBand24;
-filter25 = filterBand25;
-filter26 = filterBand26;
-filter27 = filterBand27;
-filter28 = filterBand28;
-filter29 = filterBand29;
-filter30 = filterBand30;
-filter31 = filterBand31;
-filter32 = filterBand32;
-filter33 = filterBand33;
-filter34 = filterBand34;
-filter35 = filterBand35;
-filter36 = filterBand36;
-filter37 = filterBand37;
-filter38 = filterBand38;
-filter39 = filterBand39;
-%% Sound reader (for now)
-audioInput = dsp.AudioFileReader('Filename', 'B_eng_f2.wav');  
+%% Sound reader
+audioInput = dsp.AudioFileReader('Filename', 'B_eng_f1.wav');  
 audioWriter = audioDeviceWriter('SampleRate',audioInput.SampleRate);
 
 %% Amplification
 % To have a measure of reference, play the original audio once
- while ~isDone(audioInput)
-     input = audioInput();  % Load a frame
-     audioWriter(input);  % Play the frame
- end
-%Wait until audio is played to the end
-
+%  while ~isDone(audioInput)
+%      input = audioInput();  % Load a frame
+%      audioWriter(input);  % Play the frame
+%  end
+% %Wait until audio is played to the end
+% 
 release(audioInput);       % Close input file
 release(audioWriter);               % Close audio output device
 
 SpecAna = dsp.SpectrumAnalyzer('PlotAsTwoSidedSpectrum',false, ...
-    'SampleRate',audioInput.SampleRate, ...
+    'SampleRate',40000, ...
     'NumInputPorts',2,...
     'ShowLegend',true, ...
-    'YLimits',[-145,45]);
+    'YLimits',[-145,45] );
 
 SpecAna.ChannelNames = {'Original signal','Amplified signal'};
 counter = 1;
 
-while ~isDone(audioInput)
-    input = audioInput();  % Load a frame of audio
-    tic
-     a = filter24(input);
-     a = a * audiogram(1);
-     b = filter25(input);
-     b = b * audiogram(2);
-     c = filter26(input);
-     c = c * audiogram(3);
-     d = filter27(input);
-     d = d * audiogram(4);
-     e = filter28(input);
-     e = e * audiogram(5);
-     f = filter29(input);
-     f = f * audiogram(6);
-     g = filter30(input);
-     g = g * audiogram(7);
-     h = filter31(input);
-     h = h * audiogram(8);
-     i = filter32(input);
-     i = i * audiogram(9);
-     j = filter33(input);
-     j = j * audiogram(10);
-     k = filter34(input);
-     k = k * audiogram(11);
-     l = filter35(input);
-     l = l * audiogram(12);
-     m = filter36(input);
-     m = m * audiogram(13);
-     n = filter37(input);
-     n = n * audiogram(14);
-     o = filter38(input);
-     o = o * audiogram(15);
-     p = filter39(input);
-     p = p * audiogram(16);
+samplingRate = 40*10^3;
 
-     reconstructed = a+ b+ c+ d +e+ f+ g+ h+ i+ j+ k+ l+ m+ n+ o+ p;
-     endOfFiltering(counter) = toc ;
-     % Play the reconstructed audio frame
-     audioWriter(reconstructed);
-     %SpecAna(input, reconstructed);
-     counter = counter+1;
+Sine1 = dsp.SineWave('SampleRate',samplingRate,'Frequency',250);
+% Sine2 = dsp.SineWave('Frequency',300,'SampleRate',samplingRate);
+% Sine3 = dsp.SineWave('Frequency',500,'SampleRate',samplingRate);
+
+Sine1.SamplesPerFrame = 4000;
+%Sine2.SamplesPerFrame = 4000;
+%Sine3.SamplesPerFrame = 4000;
+
+
+for i = 1 : 2
+    x = Sine1();
+    output = filterFrames(x, audiogram);
+
+    % FFT for the output
+    n=length(output);     
+    xFourier = fft(output-mean(output))/n; %
+    xFourier = 2*xFourier(1:length(output)/2+1);    
+    magnitudeFiltered = 20*log10(abs(xFourier)); % 
+    freqNew =linspace(0,Fs/2,length(output)/2+1); % 
     
+    % FFT for the input
+    nOriginal=length(buffer); %
+    originalFourier = fft(buffer-mean(buffer))/nOriginal; %
+    originalFourier = 2*originalFourier(1:length(buffer)/2+1); %
+    magnitudeOriginal = 20*log10(abs(originalFourier)); %     
+    freqOld = linspace(0,Fs/2,length(buffer)/2+1); %     %cla % 
+    
+     plot(freqNew, magnitudeFiltered, 'b'); % 
+    drawnow % 
+    
+    
+    hold on 
+      plot(freqOld, magnitudeOriginal,'r'); %             
+    drawnow           
+    hold off
 end
+release(SpecAna)
 
-mean(endOfFiltering) % to find the average time of all the loops
+
+
+% figure(1)
+% while ~isDone(audioInput)
+%     
+%     buffer = audioInput();  % Load a frame of audio
+%     %tic
+%     output = filterFrames(buffer, audiogram);
+%     %endOfFiltering(counter) = toc ;
+%     % Play the reconstructed audio frame
+%    % audioWriter(output);
+%     SpecAna(buffer, output);
+% %     counter = counter+1;
+% %     n=length(output);   % create a scaling variable equal to the 
+% %                             % length of the data
+% %                     
+% %     xFourier = fft(output-mean(output))/n; % fourier transform of the data
+% %     xFourier = 2*xFourier(1:length(output)/2+1); % create a one sided
+% %                                                     % frequency spectrum                                     
+% %     magnitudeFiltered = 20*log10(abs(xFourier));
+% %     freqx = linspace(0,Fs/2,length(output)/2+1);
+% %     nOriginal=length(buffer); 
+% %     originalFourier = fft(output-mean(output))/nOriginal;
+% %     originalFourier = 2*originalFourier(1:length(buffer)/2+1); 
+% %     magnitudeOriginal = 20*log10(abs(originalFourier));
+% %     freqx2 = linspace(0,Fs/2,length(buffer)/2+1);
+% %     %cla
+% %             plot(freqx2, magnitudeOriginal,'r');
+% %             drawnow
+% %             hold on
+% %             plot(freqx, magnitudeFiltered, 'b');
+% %             drawnow
+% %             hold off
+%             
+% 
+% end
+

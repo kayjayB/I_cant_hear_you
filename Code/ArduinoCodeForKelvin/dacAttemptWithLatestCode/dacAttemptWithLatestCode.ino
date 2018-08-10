ea8dfba280fece6fd10285ad62c8e2df24a86ab8
kdasilva835842 <kelvinkhali@gmail.com>
2018-08-10 15:29:55 +0200

    DAC, just work....

--- Code/ArduinoCodeForKelvin/dacAttemptWithLatestCode/dacAttemptWithLatestCode.ino
+++ Code/ArduinoCodeForKelvin/dacAttemptWithLatestCode/dacAttemptWithLatestCode.ino
@@ -25,14 +25,18 @@
 #include <math.h>
 
 static void argInit_50x4_real_T(double result[200]);
+static void argInit_100_real_T(unsigned int result[100]);
 int directionalityAngle(volatile int x);
 
+const int ADC_FREQ = 100000;
 const int sampleCount = 400; //50*8
-double Fs = 18000;
+double Fs = 44117;
 
 volatile double input[sampleCount];
 double inputVector[sampleCount];
 volatile int sample_counter = 0;
+volatile int dac_counter = 0;
+volatile int number_of_interrupts = 0;
 volatile int potentiometerValue = 0;
 volatile bool alternate = 1;
 unsigned int adcResult0 = 0;
@@ -44,11 +48,13 @@ unsigned int adcResult5 = 0;
 unsigned int adcResult6 = 0;
 unsigned int adcResult7 = 0;
 unsigned int adcResult8 = 0;
+unsigned int adcResult9 = 0;
 
 double directionalOutput[400];
 double outputAmplification[400];
 double result[50];
 double compressedOutput[50];
+unsigned int calibration[100];
 float ADC_value1[5];
 float ADC_value2[5];
 float outputFilter12[5];
@@ -64,6 +70,7 @@ float sum;
 int ADC_counter = 7;
 double weightings[8];
 long t0, t;
+double offset = 0;
 
 const double weightTable[19][8] = { { -0.000437317784256560, -0.000437317784256560, -0.000291545189504373, -0.000291545189504373, -0.000145772594752187, -0.000145772594752187, 0, 0},
   { -0.000430673944465980, -0.000430673944465980, -0.000287115962977320, -0.000287115962977320, -0.000143557981488660, -0.000143557981488660, 0, 0},
@@ -100,60 +107,139 @@ static void argInit_50x4_real_T(double result[400])
   }
 }
 
-void setup() {
-  Serial.begin(115200); // Begin Serial port
+static void argInit_100_real_T(unsigned int result[100])
+{
+  int idx0;
 
-  pmc_enable_periph_clk (ID_ADC);
+  // Loop over the array to initialize each element.
+  // 4 rows and 50 columns
+  for (idx0 = 0; idx0 < 100; idx0++) {
+      result[idx0] = 0;
+  }
+}
 
-  adc_init (ADC, SystemCoreClock, ADC_FREQ_MIN*2, ADC_STARTUP_FAST);
+static void argInit_100_real_T(double compressedOutput[50])
+{
+  int idx0;
 
-  ADC->ADC_WPMR = 0x00;//Disables the write protect key, WPEN
-  ADC->ADC_MR = 0x00000000;//clear all the before setted characteristics of ADC 
+  // Loop over the array to initialize each element.
+  // 4 rows and 50 columns
+  for (idx0 = 0; idx0 < 50; idx0++) {
+      compressedOutput[idx0] = 0;
+  }
+}
 
-  PIOA->PIO_PDR |= PIO_PDR_P16; //Disable PIO Controller
-  ADC->ADC_MR = ADC_MR_PRESCAL(2); // set ADC prescale to 2
-  ADC->ADC_MR |= 0x80;  //set free running mode on ADC. Don't wait for triggers
-  //ADC->ADC_CHER = 0xF0; //enable ADC on pin A0-A3 // for A0 to A8 use 0x4FF
-  //ADC->ADC_CHER = 0xFF; //enable ADC on pin A0-A7 // for A0 to A8 use 0x4FF
-  ADC->ADC_CHER = 0xF8; //enable ADC on pin A0-A4 // for A0 to A8 use 0x4FF
+void setup()
+{
+ Serial.begin(115200);
 
-  ADC->ADC_MR |= ADC_MR_TRACKTIM(3); 
-  ADC->ADC_MR |= ADC_MR_STARTUP_SUT8; 
-  ADC->ADC_EMR = 0;
-  REG_ADC_MR = (REG_ADC_MR & 0xFFF0FFFF) | 0x00020000;
-  ADC->ADC_MR |= 0x40; // Set fast wakeup mode
-  ADC->ADC_MR |= ADC_MR_LOWRES_BITS_12;
-  adc_disable_ts(ADC);
+  argInit_100_real_T(calibration);
 
-  //Interupt Setup
-  NVIC_SetPriority(ADC_IRQn, 3);    
-  adc_disable_interrupt(ADC, 0xFFFFFFFF);
-  adc_enable_interrupt(ADC,ADC_IER_EOC7);
-  NVIC_EnableIRQ(ADC_IRQn);  
+  analogReadResolution(12); 
+  pinMode(A11, INPUT);
 
-  // DAC Setup
-  // DAC Setup
-  PMC->PMC_PCER1 = PMC_PCER1_PID38;     // DACC power ON
-  DACC->DACC_CR = DACC_CR_SWRST ;       // Reset DACC
-  DACC->DACC_MR = DACC_MR_TRGEN_DIS                    // Free running mode
-                  | DACC_MR_USER_SEL_CHANNEL1          // select channel 1
-                  | DACC_MR_REFRESH (1)
-                  | DACC_MR_STARTUP_8
-                  | DACC_MR_MAXS_MAXIMUM;
+  for (int i=0; i<100;i++)
+  {
+    //while ((ADC->ADC_ISR & 0xFF) != 0xFF);
+    calibration[i] = analogRead(A11);
+   // Serial.println(calibration[i]);
+    delay(5);
+  }
+  for (int i=0; i<100; i++) 
+  {
+    offset += calibration[i];
+  }
+  offset = offset/100; // average
+  // t0 = micros();
+  offset = offset*0.00080586; 
+  //Serial.println(offset, 8);
+  
+ adc_setup () ;         // setup ADC
+ 
+ pmc_enable_periph_clk (TC_INTERFACE_ID + 0*3+0) ;  // clock the TC0 channel 0
 
-  //DACC->DACC_IER |= DACC_IER_EOC;
+ TcChannel * t = &(TC0->TC_CHANNEL)[0] ;    // pointer to TC0 registers for its channel 0
+ t->TC_CCR = TC_CCR_CLKDIS ;  // disable internal clocking while setup regs
+ t->TC_IDR = 0xFFFFFFFF ;     // disable interrupts
+ t->TC_SR ;                   // read int status reg to clear pending
+ t->TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK1 |   // use TCLK1 (prescale by 2, = 42MHz)
+             TC_CMR_WAVE |                  // waveform mode
+             TC_CMR_WAVSEL_UP_RC |          // count-up PWM using RC as threshold
+             TC_CMR_EEVT_XC0 |     // Set external events from XC0 (this setup TIOB as output)
+             TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_CLEAR |
+             TC_CMR_BCPB_CLEAR | TC_CMR_BCPC_CLEAR ;
 
-  //NVIC_EnableIRQ(DACC_IRQn);               // Enable DACC interrupt
-  DACC->DACC_CHER = DACC_CHER_CH1;      // enable channel 1 = DAC1
+ //t->TC_RC =  437 ;     // counter resets on RC, so sets period in terms of 42MHz clock - 96kHz
+ //t->TC_RA =  218 ;     // Duty cycle
+//t->TC_RC =  875 ;     // counter resets on RC, so sets period in terms of 42MHz clock - 48kHz
+//t->TC_RA =  440 ; 
+t->TC_RC =  952 ;     // counter resets on RC, so sets period in terms of 42MHz clock - 44.1kHz
+t->TC_RA =  476 ; 
+ t->TC_CMR = (t->TC_CMR & 0xFFF0FFFF) | TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_SET ;  // set clear and set from RA and RC compares
+ 
+ t->TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG ;  // re-enable local clocking and switch to hardware trigger source.
+
+ setup_pio_TIOA0() ;  // drive Arduino pin 2 at 48kHz to bring clock out
+
+ dac_setup();
+
+}
+
+void setup_pio_TIOA0 ()  // Configure Ard pin 2 as output from TC0 channel A (copy of trigger event)
+{
+  PIOB->PIO_PDR = PIO_PB25B_TIOA0 ;  // disable PIO control
+  PIOB->PIO_IDR = PIO_PB25B_TIOA0 ;   // disable PIO interrupts
+  PIOB->PIO_ABSR |= PIO_PB25B_TIOA0 ;  // switch to B peripheral
+}
+
+void adc_setup ()
+{
+ NVIC_EnableIRQ (ADC_IRQn) ;   // enable ADC interrupt vector
+ ADC->ADC_IDR = 0xFFFFFFFF ;   // disable interrupts
+ ADC->ADC_IER = 0x80 ;         // enable AD7 End-Of-Conv interrupt (Arduino pin A0)
+ ADC->ADC_CHDR = 0xFFFF ;      // disable all channels
+ ADC->ADC_CHER = 0x4FF ;        // ch7:A0 ch6:A1 ch5:A2 ch4:A3 ch3:A4 ch2:A5 ch1:A6 ch0:A7
+ ADC->ADC_CGR = 0x15555555 ;   // All gains set to x1
+ ADC->ADC_COR = 0x00000000 ;   // All offsets off
+ 
+ ADC->ADC_MR = (ADC->ADC_MR & 0xFFFFFFF0) | (1 << 1) | ADC_MR_TRGEN ;  // 1 = trig source TIO from TC0
+}
+
+void dac_setup()
+{
+    // DAC Setup
+//  analogWrite(DAC1, 0);
+//  analogWriteResolution(12);
+//  pinMode(10, OUTPUT);
+//  pinMode(11, OUTPUT);
+//  pinMode(12, OUTPUT);
+  pmc_enable_periph_clk (DACC_INTERFACE_ID) ; // start clocking DAC
+  DACC->DACC_CR = DACC_CR_SWRST ;  // reset DAC
+
+  DACC->DACC_MR = 
+    DACC_MR_TRGEN_EN | DACC_MR_TRGSEL (1) |  // trigger 1 = TIO output of TC0
+    (0 << DACC_MR_USER_SEL_Pos) |  // select channel 0
+    DACC_MR_REFRESH (0x0F) |       // bit of a guess... I'm assuming refresh not needed at 48kHz
+    (24 << DACC_MR_STARTUP_Pos) ;  // 24 = 1536 cycles which I think is in range 23..45us since DAC clock = 42MHz
+
+  DACC->DACC_IDR = 0xFFFFFFFF ; // no interrupts
+  DACC->DACC_CHER = DACC_CHER_CH1 << 0 ; // enable chan0
+
+
 
   argInit_50x4_real_T(directionalOutput);
   argInit_50x4_real_T(outputAmplification);
-
-  t0 = micros();
+  argInit_50x4_real_T(compressedOutput);
 }
 
-void loop() {
+void dac_write (int val)
+{
+  DACC->DACC_CDR = val ;
+}
 
+void loop()
+{
+    //t0 = micros();
 
   // Serial.print("Time per sample: ");
   // Serial.println((float)t/400);
@@ -161,16 +247,23 @@ void loop() {
   // Serial.println((float)400*1000000/t);
   // Serial.println();
   // delay(1000);
-
-  //angle = directionalityAngle(potentiometerValue);
-  angle = 9;
-  memcpy(weightings, weightTable[angle], 8 * sizeof(double));
+  angle = directionalityAngle(potentiometerValue);
+  memcpy(weightings, weightTable[9], 8 * sizeof(double));
 
   for (int j = 0; j < sampleCount; j++) inputVector[j] = input[j];
 
-  // for (int idx0 = 0; idx0 < 400; idx0 = idx0 + 8) {
-  //  Serial.println(inputVector[idx0]);
-  // }
+   for (int idx0 = 0; idx0 < 400; idx0 = idx0 + 8) {
+    int i=0;
+    compressedOutput[i]=inputVector[idx0];
+//    Serial.print(inputVector[idx0], 6);
+//    Serial.print(",");
+//    Serial.print(inputVector[idx0+2], 6);
+//    Serial.print(",");
+//    Serial.print(inputVector[idx0+4], 6);
+//    Serial.print(",");
+//    Serial.println(inputVector[idx0+6], 6);
+      i=1+1;
+   }
 
   // for (int idx0 = 0; idx0 < 8; idx0++) {
   //   Serial.print("Row in weight table: ");
@@ -180,122 +273,121 @@ void loop() {
   // }
 
 
-  timeDelay(inputVector, weightings, Fs, directionalOutput);
-
-  // Apply gains to the first filter signals
-  for (int j = 0; j < 400; j = j + 2) {
-    outputAmplification[j] = directionalOutput[j]*gain12;
-  }
-  
-  // Apply gains to the second filter signals
-  for (int j = 1; j < 400; j = j + 2) {
-    outputAmplification[j] = directionalOutput[j]*gain15;
-  }
-
-  // Adding the microphone signals together
-  for (int idx1 = 0; idx1 < 50; idx1++) {
-    double temp = 0;
-    for (int idx0 = 0; idx0 < 8; idx0++) {
-      temp += outputAmplification[idx0 + 8 * idx1] ;
-    }
-    result[idx1] = temp/4; // Divide by 4 to create the correct amplitude
-  }
-
-  rangeCompression(result, Fs, compressedOutput);
-
-  //   for (int idx0 = 0; idx0 < 50; idx0++) {
-  //     compressedOutput[idx0] = ((result[idx0]+ 1.5875)*4095/3.3);
-  //     Serial.print(angle);
-  //     // Serial.print(result[idx0]);
-  //     Serial.print(",");
-  //     Serial.println(compressedOutput[idx0]);
-  // }
+//  timeDelay(inputVector, weightings, Fs, directionalOutput);
+//
+//  // Apply gains to the first filter signals
+//  for (int j = 0; j < 400; j = j + 2) {
+//    //outputAmplification[j] = directionalOutput[j]*gain12;
+//    outputAmplification[j] = directionalOutput[j]*1;
+//  }
+//  
+//  // Apply gains to the second filter signals
+//  for (int j = 1; j < 400; j = j + 2) {
+//    //outputAmplification[j] = directionalOutput[j]*gain15;
+//    outputAmplification[j] = directionalOutput[j]*1;
+//  }
+//
+//  // Adding the microphone signals together
+//  for (int idx1 = 0; idx1 < 50; idx1++) {
+//    double temp = 0;
+//    for (int idx0 = 0; idx0 < 8; idx0++) {
+//      temp += outputAmplification[idx0 + 8 * idx1] ;
+//    }
+//    result[idx1] = temp/4; // Divide by 4 to create the correct amplitude
+//  }
+//
+//  rangeCompression(result, Fs, compressedOutput);
+//
+//  for (int idx0 = 0; idx0 < 50; idx0++) {
+//    compressedOutput[idx0] = ((compressedOutput[idx0]+ offset)*4095/3.3);
+//  //  Serial.print(angle);
+//  //  // Serial.print(result[idx0]);
+//  //  Serial.print(",");
+//  //  Serial.println(compressedOutput[idx0]);
+//}
   
   //      dacc_write_conversion_data(DACC_INTERFACE, sum);
-  for (int i=0; i<50; i++){
-    DACC->DACC_ISR;
-    DACC->DACC_CDR = compressedOutput[i];
-  }
   sample_counter = 0;
-  delay(10);
+  dac_counter = 0;
+  
+
+  // t = micros()-t0;  // calculate elapsed time
+  // Serial.print("Frequency: ");
+  // Serial.println((float)9*number_of_interrupts*1000000/t);
+  // Serial.println();
+
+  number_of_interrupts = 0;
 }
 
-// ADC Interrupt handler.
-void ADC_Handler(void)
-{ 
-  if ((adc_get_status(ADC) & ADC_IER_EOC7) == ADC_IER_EOC7) 
-  { 
-    adc_disable_interrupt(ADC, ADC_IER_EOC7);
+void ADC_Handler (void)
+{
+ //wait untill all 8 ADCs have finished thier converstion.
+ while(!((ADC->ADC_ISR & ADC_ISR_EOC7) && (ADC->ADC_ISR & ADC_ISR_EOC6) && (ADC->ADC_ISR & ADC_ISR_EOC5) && (ADC->ADC_ISR & ADC_ISR_EOC4)
+  && (ADC->ADC_ISR & ADC_ISR_EOC3) && (ADC->ADC_ISR & ADC_ISR_EOC2) && (ADC->ADC_ISR & ADC_ISR_EOC1) && (ADC->ADC_ISR & ADC_ISR_EOC0)));
 
-    // Read in all the ADC values - 8 channels
-    // adcResult0 = ADC->ADC_CDR[7];
-    // adcResult1 = ADC->ADC_CDR[6];
-    // adcResult2 = ADC->ADC_CDR[5];
-    // adcResult3 = ADC->ADC_CDR[4];
-    // adcResult4 = ADC->ADC_CDR[3];
-    // adcResult5 = ADC->ADC_CDR[2];
-    // adcResult6 = ADC->ADC_CDR[1];
-    // adcResult7 = ADC->ADC_CDR[0];
+    adcResult0 = ADC->ADC_CDR[7];
+    adcResult1 = ADC->ADC_CDR[6];
+    adcResult2 = ADC->ADC_CDR[5];
+    adcResult3 = ADC->ADC_CDR[4];
+    adcResult4 = ADC->ADC_CDR[3];
+    adcResult5 = ADC->ADC_CDR[2];
+    adcResult6 = ADC->ADC_CDR[1];
+    adcResult7 = ADC->ADC_CDR[0];
+    adcResult9 = ADC->ADC_CDR[10]; //pot
 
-    // Read in all the ADC values - 4 channels
+        // Read in all the ADC values - 4 channels
     // adcResult0 = ADC->ADC_CDR[7];
     // adcResult1 = ADC->ADC_CDR[6];
     // adcResult2 = ADC->ADC_CDR[5];
     // adcResult3 = ADC->ADC_CDR[4];
 
     // Read in all the ADC values - 4 channels + pot on channel A4
-    adcResult0 = ADC->ADC_CDR[7];
-    adcResult1 = ADC->ADC_CDR[6];
-    adcResult2 = ADC->ADC_CDR[5];
-    adcResult3 = ADC->ADC_CDR[4];
-    adcResult4 = ADC->ADC_CDR[3];
+    // adcResult0 = ADC->ADC_CDR[7];
+    // adcResult1 = ADC->ADC_CDR[6];
+    // adcResult2 = ADC->ADC_CDR[5];
+    // adcResult3 = ADC->ADC_CDR[4];
+    // adcResult4 = ADC->ADC_CDR[3];
 
-    if (alternate && sample_counter < sampleCount)
+    if (sample_counter < sampleCount)
     {
-      // 8 channels
+      // 8 channels 
+      // to voltage
+      input[0 + sample_counter] = adcResult0*0.00080586 - offset;
+      input[1 + sample_counter] = adcResult1*0.00080586 - offset;
+      input[2 + sample_counter] = adcResult2*0.00080586 - offset;
+      input[3 + sample_counter] = adcResult3*0.00080586 - offset;
+      input[4 + sample_counter] = adcResult4*0.00080586 - offset;
+      input[5 + sample_counter] = adcResult5*0.00080586 - offset;
+      input[6 + sample_counter] = adcResult6*0.00080586 - offset;
+      input[7 + sample_counter] = adcResult7*0.00080586 - offset;
+
+      // 4 channels
       // input[0 + sample_counter] = adcResult0*0.00080586 - 1.5875;
       // input[1 + sample_counter] = adcResult1*0.00080586 - 1.5875;
       // input[2 + sample_counter] = adcResult2*0.00080586 - 1.5875;
       // input[3 + sample_counter] = adcResult3*0.00080586 - 1.5875;
-      // input[4 + sample_counter] = adcResult4*0.00080586 - 1.5875;
-      // input[5 + sample_counter] = adcResult5*0.00080586 - 1.5875;
-      // input[6 + sample_counter] = adcResult6*0.00080586 - 1.5875;
-      // input[7 + sample_counter] = adcResult7*0.00080586 - 1.5875;
-
-      // 4 channels
-      input[0 + sample_counter] = adcResult0*0.00080586 - 1.5875;
-      input[1 + sample_counter] = adcResult1*0.00080586 - 1.5875;
-      input[2 + sample_counter] = adcResult2*0.00080586 - 1.5875;
-      input[3 + sample_counter] = adcResult3*0.00080586 - 1.5875;
-      input[4 + sample_counter] = adcResult0*0.00080586 - 1.5875;
-      input[5 + sample_counter] = adcResult1*0.00080586 - 1.5875;
-      input[6 + sample_counter] = adcResult2*0.00080586 - 1.5875;
-      input[7 + sample_counter] = adcResult3*0.00080586 - 1.5875;
+      // input[4 + sample_counter] = adcResult0*0.00080586 - 1.5875;
+      // input[5 + sample_counter] = adcResult1*0.00080586 - 1.5875;
+      // input[6 + sample_counter] = adcResult2*0.00080586 - 1.5875;
+      // input[7 + sample_counter] = adcResult3*0.00080586 - 1.5875;
 
       // pot 
-      potentiometerValue = adcResult4;
+      potentiometerValue = adcResult9;
+
+//      dacc_set_channel_selection(DACC_INTERFACE, 1);       //select DAC channel 1
+//      dacc_write_conversion_data(DACC_INTERFACE, compressedOutput[dac_counter]);//write on DAC
+      dac_write(compressedOutput[dac_counter]);
 
       sample_counter+=8;
+      dac_counter+=1;
       alternate = 0;
 
+     
     } 
-    else if(sample_counter < sampleCount)
-    {
-      alternate = 1;
-    }
-    if (sample_counter >= sampleCount)
-    {
-      t = micros()-t0;  // calculate elapsed time
-      Serial.print("Time per sample: ");
-      Serial.println((float)t/400);
-      Serial.print("Frequency: ");
-      Serial.println((float)400*1000000/t);
-      Serial.println();
-      //t0 = micros();
-
-    }
-  }
-  adc_enable_interrupt(ADC,ADC_IER_EOC7);
+//    else if(sample_counter < sampleCount)
+//    {
+//      alternate = 1;
+//    }
 }
 
 int directionalityAngle(volatile int x)
